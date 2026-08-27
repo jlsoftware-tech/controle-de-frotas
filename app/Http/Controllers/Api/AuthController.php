4e019bc03c2d70a792dcf4f2567b4ba719c8d13d
Yago Elias <yagoelias024@gmail.com>
2026-08-27 09:12:49 -0300

    feat: adiciona o processo de redefinir senha do usuário

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,10 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Profile;
 use App\Models\Secretariat;
+use App\Notifications\ResetPasswordApiNotification;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -26,9 +30,9 @@ class AuthController extends Controller
         if (!$token) {
             return response()->json([
                 'success' => false,
-                'message' => 'Erro ao logar.',
+                'message' => 'Usuário ou senha incorretos.',
                 'data' => null,
-            ]);
+            ], Response::HTTP_UNAUTHORIZED);
         }
 
         return response()->json([
@@ -71,15 +75,93 @@ class AuthController extends Controller
                 'success' => false,
                 'message' => 'Erro ao cadastrar.',
                 'data' => null,
-            ]);
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
+        Auth::guard('api')->login($user);
         $token = Auth::guard('api')->tokenById($user->id);
         return response()->json([
             'success' => true,
             'token' => $token,
             'message' => 'Cadastro realizado com sucesso.',
-            'data' => ['user' => auth()->user()],
+            'data' => Auth::guard('api')->user(),
+        ], Response::HTTP_CREATED);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('api')->logout();
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout realizado com sucesso.',
+            'data' => null,
         ]);
+    }
+
+    public function refresh()
+    {
+        return [
+            'success' => true,
+            'token' => Auth::guard('api')->refresh(),
+            'data' => Auth::guard('api')->user()
+        ];
+    }
+
+    public function me(Request $request)
+    {
+        return response()->json([
+            'success' => true,
+            'data' => Auth::guard('api')->user()
+        ]);
+    }
+
+    public function forgotPassword(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email'),
+            function (User $user, $token) {
+                $user->notify(new ResetPasswordApiNotification($token));
+            }
+        );
+
+        return $status === Password::RESET_LINK_SENT ?
+            response()->json([
+                'success' => true,
+                'message' => 'Link enviado com sucesso.'
+            ]) :
+            response()->json([
+                'success' => false,
+                'message' => 'Não foi possível enviar o link.'
+            ]);
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|string|email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) use ($request) {
+                $user->forceFill(['password' => Hash::make($password)])->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET ?
+            response()->json([
+                'success' => true,
+                'message' => 'Sua senha foi redefinida com sucesso.'
+            ]) :
+            response()->json([
+                'success' => false,
+                'message' => 'Token inválido ou expirado.'
+            ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,15 @@ Route::prefix('v1')->group(function () {
     Route::prefix('auth')->middleware('api')->group(function() {
         Route::post('/login', [AuthController::class, 'login']);
         Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+
+        Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+        Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+
+        Route::middleware('auth:api')->group(function() {
+            Route::post('/logout', [AuthController::class, 'logout']);
+            Route::post('/me', [AuthController::class, 'me']);
+        });
     });
 
 });
