61ada0c8d1b203c71d82ccd8c60fbb9988ef88bd
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2026-08-29 17:36:35 -0300

    feat(profiles): termina operações de listagem, edição e deleção de perfis

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,33 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\ProfileCollection;
 use App\Models\Profile;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use SebastianBergmann\Diff\Exception;
+use PHPUnit\Exception;
 
 class ProfileController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
+        $profiles = Profile::query()
+            ->when($request->filled('search'), function (Builder $q) use ($request) {
+                return $q->whereLike('name', "%{$request->input('search')}%");
+            })
+            ->orderBy('name')
+            ->paginate(10);
+
         return response()->json([
             'success' => true,
             'message' => 'Lista de todos os perfis de acesso',
-            'data' => Profile::all()->toResourceCollection()
+            'data' => $profiles->toResourceCollection()
         ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
+        $request->validate([
+            'name' => ['required', 'unique:profiles', 'max:50'],
+            'description' => ['max:255']
+        ]);
 
+        try {
+            $newProfile = Profile::create([
+                'name' => $request->input('name'),
+                'description' => $request->input('description')
+            ]);
+
+            $newProfile->save();
+        }
+        catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro: perfil não foi criado',
+                'data' => null
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Perfil criado com sucesso',
+            'data' => [ $newProfile ]
+        ]);
     }
 
     /**
@@ -46,15 +77,35 @@ class ProfileController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Profile $profile)
+    public function update(Request $request, Profile $profile): JsonResponse
     {
-        //
+        $request->validate([
+            'name' => ['required', 'unique:profiles', 'max:50'],
+            'description' => ['max:255']
+        ]);
+
+        try {
+            $profile->update($request->toArray());
+        }
+        catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro: perfil não foi atualizado',
+                'data' => null
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Perfil atualizado',
+            'data' => [ $profile ]
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Profile $profile)
+    public function destroy(Profile $profile): JsonResponse
     {
         try {
             $profile->deleteOrFail();
