9fb37446d8e47693d85ab24a5580f5ec3b2192a0
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2026-09-13 00:00:55 -0300

    feat: implementação de migrations e models de medições e documentos de veículo

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Vehicle extends Model
 {
     public function secretariat(): BelongsTo
     {
         return $this->belongsTo(Secretariat::class);
+    }
+
+    public function documents(): HasMany
+    {
+        return $this->hasMany(VehicleDocument::class);
+    }
+
+    public function measures(): HasMany
+    {
+        return $this->hasMany(VehicleDocument::class);
     }
 }
