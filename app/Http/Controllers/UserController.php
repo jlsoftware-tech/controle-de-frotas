16257c7236b8561680c43abfa4bb211d8f432962
Yago-Elias <53307475+Yago-Elias@users.noreply.github.com>
2026-09-20 03:02:42 +0000

    style: [ci] corrige formatação do código

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,6 @@ use App\Models\User;
 use App\Support\ApiResponder;
 use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Knuckles\Scribe\Attributes\Endpoint;
